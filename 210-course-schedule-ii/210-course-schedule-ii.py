a8622268f162ab31910e7181301d3917cd27319e
Gedion Teshome <Teshomegedion@gmail.com>
2022-04-14 17:47:07 +0300

    Time: 106 ms (84.90%), Space: 15.5 MB (88.30%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+         
+        graph = {}
+        indegree = {}
+        
+        
+        for i in range(numCourses):
+            graph[i] = []
+            indegree[i] = 0
+            
+        
+        for edge in prerequisites:
+            child = edge[0]
+            parent = edge[1]
+            
+            indegree[child] += 1
+            graph[parent].append(child)
+            
+            
+        sources = deque()
+        
+        for i,j in indegree.items():
+            if j==0:
+                sources.append(i)
+        ans=[]
+        
+        while sources:
+            c = sources.popleft()
+            ans.append(c)
+            
+            for nbr in graph[c]:
+                indegree[nbr] -= 1
+                
+                if indegree[nbr] == 0:
+                    sources.append(nbr)
+        
+        if len(ans) == numCourses:
+            return ans
+        return []
