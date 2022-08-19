d7bc7944cdbe7e46b84dfeacc1df6540e3e893a6
Gedion Teshome <Teshomegedion@gmail.com>
2022-08-19 11:35:20 +0300

    Time: 910 ms (62.31%), Space: 27.8 MB (91.47%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,24 +1,19 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # floyd's algorithm for cycle detection
+        # -O
         
-        def check(n):
-            count = 0
-            for i in nums:
-                if i <= n:
-                    count += 1
-            return count <= n    
+        slow, fast = 0, 0
         
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
         
-        l, r = 1, len(nums)
-        ans = 0
-        
-        while l <= r:
-            mid = l + (r - l)//2
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
             
-            if not check(mid):
-                ans = mid
-                r = mid-1
-            else:
-                l = mid+1
-            
-        return ans
+        return slow
