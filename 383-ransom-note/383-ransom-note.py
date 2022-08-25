3b2e624cd242b96831e74af6a8e4fae053cdebf6
Gedion Teshome <Teshomegedion@gmail.com>
2022-08-25 08:00:42 +0300

    Time: 64 ms (84.00%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,16 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        r_count = Counter(ransomNote)
+        m_count = Counter(magazine)
         
-        magAlpha = defaultdict(int)
-        
-        for char in magazine:
-            magAlpha[char] += 1
-            
-        for char in ransomNote:
-            if magAlpha[char] >= 1:
-                magAlpha[char] -= 1
-            else:
+        for i in r_count:
+            if m_count[i] < r_count[i] or i not in m_count:
                 return False
         
         return True
