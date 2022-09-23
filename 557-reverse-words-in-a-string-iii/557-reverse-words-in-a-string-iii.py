d1d426fdf12d496b8d44f9dc4475d016f83b4b84
Gedion Teshome <Teshomegedion@gmail.com>
2022-09-23 05:39:08 +0300

    Time: 348 ms (5.03%), Space: 14.6 MB (82.00%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,10 +1,18 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        new = ""
-        for w in s.split(" "):   
-            new += w[::-1] + " "
-        return new[:-1]
+        def flip_word_in_place(left_p:int, right_p: int):
+            while left_p < right_p:
+                s_arr[left_p], s_arr[right_p] = s_arr[right_p], s_arr[left_p]
+                left_p += 1
+                right_p -= 1
+
+        left = 0
+        s_arr = list(s)
         
+        for i in range(len(s)):
+            if s_arr[i] == " ":
+                flip_word_in_place(left, i - 1)
+                left = i + 1
+        flip_word_in_place(left, len(s_arr)-1)
         
-            
-        
+        return "".join(s_arr)
