ad9903d783c2b1be9d1353c313b2156d74fc4750
Gedion Teshome <Teshomegedion@gmail.com>
2022-09-27 15:23:28 +0300

    Time: 108 ms (85.50%), Space: 13.9 MB (34.91%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,14 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        boardMap = collections.defaultdict(list)
-        for x in range(9):
-            for y in range(9):
-                char = board[x][y]
-                if char != '.': 
-                    if char in boardMap:
-                        for pos in boardMap[char]:
-                            if (pos[0]== x) or (pos[1] == y) or (pos[0]//3 == x//3 and pos[1]//3 == y//3):
-                                return False
-                    boardMap[char].append((x,y))
-   
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set) # key = (row/3, col/3)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+                
         return True
