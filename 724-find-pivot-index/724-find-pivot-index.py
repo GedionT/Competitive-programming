6fc0fb5ea590521c7d05f8e9ba5d666fa4b81754
Gedion Teshome <Teshomegedion@gmail.com>
2022-09-28 16:52:01 +0300

    Time: 367 ms (21.41%), Space: 15.2 MB (91.73%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,20 +1,18 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
+        
         ltr_pre = [0]
         rtl_pre = [0]
         
-        for idx in range(len(nums)): 
+        for idx in range(len(nums)-1): 
             ltr_pre.append(nums[idx]+ltr_pre[-1])
             
-        for idx in range(len(nums)-1, -1, -1):
+        for idx in range(len(nums)-1, 0, -1):
             rtl_pre.append(nums[idx]+rtl_pre[-1])
         
         rtl_pre.reverse()
-        
-        ltr_pre = ltr_pre[1:]
-        rtl_pre = rtl_pre[0:len(rtl_pre)-1]
-        
+
         for i in range(len(ltr_pre)):
             if ltr_pre[i] == rtl_pre[i]:
                 return i
