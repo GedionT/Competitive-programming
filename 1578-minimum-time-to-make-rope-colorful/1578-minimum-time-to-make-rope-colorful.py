6611a82e43cf1dd819e2982cd893911f8547b42f
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-03 07:24:29 +0300

    Time: 3425 ms (5.05%), Space: 24.9 MB (52.92%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,12 +1,18 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        
-        run_sum = 0
-        for i in range(1, len(colors)):
-            if colors[i] == colors[i-1]:
-                run_sum += min(neededTime[i], neededTime[i-1])
-                neededTime[i] = max(neededTime[i], neededTime[i-1])
-        
-        return run_sum
     
-    
+        tot_time = left = right = 0
+        
+        while left < len(neededTime) and right < len(neededTime):
+            curr_total = curr_max = 0
+            
+            while right < len(neededTime) and colors[left] == colors[right]:
+                curr_total += neededTime[right]
+                curr_max = max(curr_max, neededTime[right])
+                right += 1
+                
+            # end of curr group, add cost of group to tot tie and reset ptrs
+            tot_time += curr_total - curr_max
+            left = right
+            
+        return tot_time
