76261130af6c2ac3ad28cb2caa7558ad47f81fed
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-07 06:14:37 +0300

    Time: 1341 ms (24.08%), Space: 13.9 MB (67.74%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        s1 = sorted(s1)
-         
-        for i in range(len(s2)-len(s1)+1):
-            if s1 == sorted(s2[i:i+len(s1)]):
+        len1 = len(s1)
+        mps1 = collections.Counter(s1)
+        
+        for i in range(len(s2) - len1+1):
+            sub = s2[i:i+len1]
+            if mps1 == collections.Counter(sub):
                 return True
-        
-        
         return False
