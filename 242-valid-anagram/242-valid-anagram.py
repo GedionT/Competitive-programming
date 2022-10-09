6d243cb353c159e84e0d1a3f89fa6986c6018ba5
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-09 07:48:59 +0300

    Time: 71 ms (70.79%), Space: 14.5 MB (67.10%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,4 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+# alt 1: straight forward O(nlogn)      
+#         if sorted(s) == sorted(t):
+#             return True
         
-        return sorted(s) == sorted(t)
+#         O(n) space and O(n) complexity
+        dictionary = defaultdict(int)
+        
+        for c in s: 
+            dictionary[c] += 1
+        for c in t:
+            dictionary[c] -= 1
+            
+        return all(c == 0 for c in dictionary.values())
