cbc3dca952a8786557cac6bfc325dc4bac67fea1
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-11 04:02:45 +0300

    Time: 1393 ms (30.63%), Space: 24.7 MB (48.78%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,14 +1,12 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         
-        n = len(nums)
-        stack = []
-        
-        for num in nums:
-            if stack and num <= stack[-1]: # if current number smaller than previous value in stack
-                idx = bisect_left(stack, num) # search to replace the smaller value in stack
-                stack[idx] = num # replace
-            else: 
-                stack.append(num)
-                if len(stack) == 3: return True
+        first = second = float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
         return False
