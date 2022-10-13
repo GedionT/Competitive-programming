8add5499ca08a249a7a327550f0cb61efda9d335
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-13 11:53:04 +0300

    Time: 52 ms (85.94%), Space: 14.4 MB (88.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,36 +1,28 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n, m = len(matrix), len(matrix[0])
         
-        def bin_search(target, mat):
-            l, r = 0, len(mat)-1
+        find_val = lambda x, y: x*m+y
+        find_cord = lambda x: (x//m, x%m)
+        
+        l, r = 0, find_val(len(matrix)-1, len(matrix[0])-1)
+        
+        while l <= r:
+            mid = (l+r)//2
+            coord = find_cord(mid)
             
-            while l <= r:
-                mid = l + (r - l) // 2
-
-                if target > mat[mid][0]:
-                    l = mid+1
-                elif target < mat[mid][0]:
-                    r = mid-1 
-                else:
-                    return True
-                    
-                    
-            sub = mat[r]
-            l, r = 0, len(sub)-1
-            
-            while l <= r:
-                mid = l + (r - l) // 2
+            if target == matrix[coord[0]][coord[1]]:
+                return True
                 
-                if target == sub[mid]:
-                    return True
-                elif target < sub[mid]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
+            elif target < matrix[coord[0]][coord[1]]:
+                r = mid-1
+            else:
+                l = mid+1
             
-            return False
-
         
-        ans = bin_search(target, matrix)
-
-        return ans
+        return False
+            
+            
+                
+            
+        
