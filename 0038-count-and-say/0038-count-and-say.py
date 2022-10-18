1a0a8de6d4d324c6b07b264dd9759f6d5bcfe139
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-18 06:46:20 +0300

    Time: 118 ms (13.80%), Space: 13.9 MB (86.43%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def helper(n: int) -> str:
+        
+            left = right = 0
+            n = list(str(n))
+            sol = ''
+
+            while right < len(n)-1:
+                if n[left] == n[right]:
+                    right += 1
+
+                if n[left] != n[right]:
+                    sub = right-left  
+                    sol += str(sub)+str(n[left])
+                    left = right
+
+            if left == right:
+                sol += str(1)+str(n[left])
+            else:
+                sol += str(right-left+1)+str(n[left])
+
+            return sol
+        
+        ans = ["1"]
+        for i in range(1, n):
+            ans.append(helper(int(ans[i-1])))
+        
+        return ans[-1]
