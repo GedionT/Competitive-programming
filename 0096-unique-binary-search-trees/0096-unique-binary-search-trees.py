788bd131022a62c8f94337e67df798fc8293d536
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-19 15:16:42 +0300

    Time: 57 ms (38.94%), Space: 13.8 MB (61.81%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,11 +1,7 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         
-        def fact(n):
-            if n == 1:
-                return 1
-      
-            return n * fact(n-1)
+        fact = math.factorial
            
         # usin catalan's number formula
         return fact(2*n) // (fact(n+1) * fact(n))
