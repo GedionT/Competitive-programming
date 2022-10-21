ff9f8e8ba5cce277fc6229ef0cb694b515547e4d
Gedion Teshome <Teshomegedion@gmail.com>
2022-10-21 04:17:10 +0300

    Time: 1587 ms (25.21%), Space: 32 MB (11.58%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -2,14 +2,14 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         count = {}
         
+        # add nums: [idxs] in dictionary
         for idx in range(len(nums)):
             if nums[idx] not in count:
                 count[nums[idx]] = [idx]
             else:
                 count[nums[idx]].append(idx)
         
-        # print(count)
-        
+        # since idxs are increasing and we need to come up with less than or k, build up on each list of idxs
         for num in count:
             for idxs in range(1, len(count[num])):
                 if abs(count[num][idxs] - count[num][idxs-1]) <= k:
