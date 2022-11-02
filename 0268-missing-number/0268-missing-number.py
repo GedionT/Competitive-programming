453496b1997b0d65a540606e3c973cd51405f858
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-02 15:38:16 +0300

    Time: 180 ms (81.98%), Space: 15.2 MB (80.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        xor1 = 0
+        xor1 = len(nums)
         xor2 = nums[0]
         
         for i in range(1, len(nums)):
             xor1 ^= i
             xor2 ^= nums[i]
         
-        return xor1 ^ xor2 ^ len(nums)
+        return xor1 ^ xor2 
             
