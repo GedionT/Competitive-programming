b5e9f2b3ae72391a027a7fc44b504f1204d25200
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-06 06:32:33 +0300

    Time: 57 ms (65.48%), Space: 13.8 MB (72.62%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.py
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -1,19 +1,7 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:        
         if k == 1:
-          
-            lex_small = s
-            s = list(s)
-
-            for idx in range(len(s)):
-                to_shift = max(s[0:k])  
-                s.pop(s.index(to_shift))
-                s.append(to_shift)
-
-                lex_small = min(lex_small, "".join(s))
-
-            return lex_small
-        
+            return min(s[i:] + s[:i] for i in range(len(s)))
         else:
-             return "".join(sorted(s))
+            return "".join(sorted(s))
 
