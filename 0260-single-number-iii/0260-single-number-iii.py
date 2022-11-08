d87383727ee3647114da9c9e6e361153e85a37f1
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-08 10:39:24 +0300

    Time: 106 ms (77.30%), Space: 15.5 MB (97.53%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,18 +1,27 @@
 class Solution:
+    def checkBit(self, n, i):
+        return (n & (1<<i))!=0
+    
+    def setBitPos(self, n):
+        # return xor & -xor
+        for i in range(32):
+            if self.checkBit(n, i):
+                return i
+    
     def singleNumber(self, nums: List[int]) -> List[int]:
         
-        ans = []
+        xor = 0        
+        for n in nums:
+            xor ^= n
         
-        s = Counter(nums)
+        setBit = self.setBitPos(xor)  
+        x1, x2 = 0, 0
         
-        for i,j in s.items():
-            if j==1:
-                ans.append(i)
-                
-        return ans
-#         xor = 0
+        for i in range(len(nums)):
+            if self.checkBit(nums[i], setBit):
+                x1^=nums[i]
+            else:
+                x2^=nums[i]
         
-#         for n in nums:
-#             xor ^= n
+        return [x1, x2]
         
-#         print(xor)
