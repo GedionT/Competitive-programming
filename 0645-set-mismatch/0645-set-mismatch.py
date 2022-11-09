f5a863a8dc417410164b750a6ed94d6cdeb942af
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-09 18:16:04 +0300

    Time: 190 ms (97.55%), Space: 15.1 MB (98.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         
-        big = small = 0
-
         hole = [0]*len(nums)
         for num in nums:
             hole[num-1] += 1
         
-        idx1 = hole.index(2)+1
-        idx2 = hole.index(0)+1
+        repeat = hole.index(2)+1
+        missing = hole.index(0)+1
         
-        return [idx1 , idx2]
+        return [repeat , missing]
         
