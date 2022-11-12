5828adea8e883f4d00a65d43a11b6bd19c34544b
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-12 09:16:47 +0300

    Time: 1741 ms (18.07%), Space: 35.8 MB (73.00%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,18 +1,32 @@
-from sortedcontainers import SortedList
-
 class MedianFinder:
 
     def __init__(self):
-        self.s = SortedList()
+        self.maxHeap = []
+        self.minHeap = []
 
     def addNum(self, num: int) -> None:
-        self.s.add(num)
+        if not self.maxHeap or num <= -self.maxHeap[0]:
+            heappush(self.maxHeap, -num)
+        else:
+            heappush(self.minHeap, num)
+
+        ### banance the two heaps so that each of them representing half of the list
+        ### for odd length list, len(maxHeap) == len(minHeap)+1
+        ### for even length list, len(maxHeap) == len(minHeap)
+        if len(self.minHeap) > len(self.maxHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap)) 
+        elif len(self.maxHeap) > len(self.minHeap)+1:
+            heappush(self.minHeap, -heappop(self.maxHeap)) 
 
     def findMedian(self) -> float:
-        l = len(self.s)
-        if l%2==0:
-            return (self.s[l//2-1]+self.s[l//2])/2
-        return self.s[l//2]
+        ### if the length of entire list is even, 
+        ### get the mean of the two middle values
+        if (len(self.maxHeap)+len(self.minHeap))%2==0:
+            return (-self.maxHeap[0]+self.minHeap[0])/2
+        
+        ### when odd, we know that the median is in maxHeap
+        return -self.maxHeap[0]
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
