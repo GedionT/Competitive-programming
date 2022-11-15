ebabe5f9cec18de6269fc5738f9158cb4e20ce58
Gedion Teshome <Teshomegedion@gmail.com>
2022-11-15 05:59:05 +0300

    Time: 85 ms (92.51%), Space: 21.4 MB (46.74%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,22 +6,19 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
         
-        res = []
+        left, right = root.left, root.right
+        lh, rh = 1, 1    
         
-        def dfs(root):
-            if not root:
-                return 0
-            
-            dfs(root.left)
-            res.append(root.val)
-            dfs(root.right)
-            
-         
-        dfs(root)
-        return len(res)
-            
-            
+        while left  : left,  lh = left.left,   lh+1           # each subtree by going all way down to
+        while right : right, rh = right.right, rh+1           # the left and right (in logN time)
+
+        if lh == rh : return ( 1 << lh ) - 1                         # if it's a full tree size is known
+        
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)   
            
         
             
