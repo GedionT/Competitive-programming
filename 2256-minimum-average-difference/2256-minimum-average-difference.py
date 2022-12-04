4dfbabb017819c97a0fa044aaa55257c40792fbb
Gedion Teshome <Teshomegedion@gmail.com>
2022-12-04 12:39:56 +0300

    Time: 3248 ms (9.76%), Space: 41 MB (5.46%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,12 +1,7 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        """
-        2, 5, 3, 9, 5, 3
-        
-        2  7  10 19  24 27
-        27 25 20  17  8  3
-        """
-        min_avg_diff = (float('inf'), 0)
+        min_avg_diff = float('inf')
+        mins = []
         
         forward_prefix = [nums[0]]
         backward_prefix = [nums[-1]]
@@ -17,19 +12,16 @@ class Solution:
         for i in range(len(nums)-2, -1, -1):
             backward_prefix.append(backward_prefix[-1] + nums[i])
         backward_prefix = backward_prefix[::-1]
-        
-        print(forward_prefix, backward_prefix)
-        
+                
         for i in range(len(nums)):
             if i+1 == len(nums):
                 curr_avg_diff = forward_prefix[i]//(i+1)
             else:
                 curr_avg_diff = abs( (forward_prefix[i]//(i+1)) - ((backward_prefix[i] - nums[i]) // (len(nums) - (i+1))) )
-            curr_avg_diff = (curr_avg_diff, i)
             min_avg_diff = min(min_avg_diff, curr_avg_diff)
-            
-        return min_avg_diff[1]    # this code will fail because won't find the smallest i
-                                  # lesson: min can work on tuple by looking at first index
-                                  # can override based on d/t key arg on the min function
+            mins.append((min_avg_diff, i))
         
+        mins.sort()
+            
+        return mins[0][1]
         
