011d0d8d5a08ca612b179c55cc17a8e19b07406c
Gedion Teshome <Teshomegedion@gmail.com>
2022-12-13 13:35:21 +0300

    Time: 381 ms (28.13%), Space: 14.9 MB (40.57%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -3,16 +3,15 @@ class Solution:
         
         for r in range(len(matrix)-2, -1, -1):
             for c in range(len(matrix)):
-                
                 best = matrix[r+1][c]
                 
                 if c > 0:
                     best = min(best, matrix[r+1][c-1])
-                
                 if c + 1 < len(matrix):
-                    best = min(best, matrix[r + 1][c+1])
+                    best = min(best, matrix[r+1][c+1])
                     
                 matrix[r][c] = matrix[r][c] + best
                 
-        
         return min(matrix[0])
+        
+       
