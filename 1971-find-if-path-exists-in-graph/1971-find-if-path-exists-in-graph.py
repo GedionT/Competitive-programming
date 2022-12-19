0582f12f918f9a0a0794dfe8746870b671773253
Gedion Teshome <Teshomegedion@gmail.com>
2022-12-19 12:27:19 +0300

    Time: 2014 ms (86.67%), Space: 103.4 MB (93.83%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,26 +1,29 @@
+class UnionFind:
+    def __init__(self, n):
+        self.root = list(range(n))
+        self.rank = [1] * n
+        
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                root_x, root_y = root_y, root_x
+                
+            self.rank[root_y] += self.rank[root_x]
+            self.root[root_x] = root_y
+        
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        graph = defaultdict(list)
-
-        for src, dest in edges:
-            graph[src].append(dest)
-            graph[dest].append(src)
+        uf = UnionFind(n)
         
-        # return self.dfs(source, destination, graph, visited)
-        
-        seen = [False]*n
-        seen[source] = True
-        queue = deque([source])
-        
-        while queue:
-            curr_node = queue.popleft()
-            if curr_node == destination:
-                return True
+        for a, b in edges:
+            uf.union(a, b)
             
-            for next_node in graph[curr_node]:
-                if not seen[next_node]:
-                    seen[next_node] = True
-                    queue.append(next_node)
-                    
-        return False
+        return uf.find(source) == uf.find(destination)
