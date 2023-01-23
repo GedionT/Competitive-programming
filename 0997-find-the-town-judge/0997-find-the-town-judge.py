09359d60957ed2654aca292c68bcf97fab07cba6
Gedion Teshome <Teshomegedion@gmail.com>
2023-01-23 11:21:58 +0300

    Time: 1045 ms (37.90%), Space: 19.1 MB (8.15%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,17 +1,17 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-        trustship = [0] * (n+1)
+        trustship = [0] * (n)
         graph = defaultdict(list)
         
         for ai, bi in trust:
             graph[ai].append(bi)
-            trustship[bi] += 1
+            trustship[bi-1] += 1
             
-        for idx in range(1, len(trustship)):
-            if trustship[idx] == len(trustship)-2:
-                if len(graph[idx]) == 0:
-                    return idx
+        for idx in range(len(trustship)):
+            if trustship[idx] == len(trustship)-1:
+                if len(graph[idx+1]) == 0:
+                    return idx+1
         
         return -1
                 
