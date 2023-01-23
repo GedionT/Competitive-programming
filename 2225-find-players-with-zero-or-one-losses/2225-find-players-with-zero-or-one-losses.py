02a7dcc69eab34e3f6352f45437e9a519f642413
Gedion Teshome <Teshomegedion@gmail.com>
2023-01-23 12:06:46 +0300

    Time: 5612 ms (8.84%), Space: 68.8 MB (60.21%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,29 +1,40 @@
 class Solution:
-    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:     
+        """
+            1: 3                
+            2: 3
+            3: 6
+            5: 6, 7
+            4: 5, 8, 9
+            10: 4, 9
+
+            WINS(len(graph))   LOSS(count in values of all keys)
+            1:  1                0
+            2:  1                0
+            3:  1                2
+            4:  3                1
+            5:  2                1
+            10: 2                0
+            6:                   2
+            7:                   1
+            8:                   1
+            9:                   2
+
+            result = [[1, 2, 10], [4, 5, 7, 8]]
+        """ 
         
-        win_count = {}
         loss_count = {}
         
-        for match in matches:
-            if match[0] in win_count:
-                win_count[match[0]] += 1
-            else:
-                win_count[match[0]] = 1
-                
-            if match[1] in loss_count:
-                loss_count[match[1]] += 1
-            else:
-                loss_count[match[1]] = 1
-                
-             
-        first = []
-        second = []
-        for win in win_count:
-            if win not in loss_count:
-                first.append(win)
+        for winner, loser in matches:
+            loss_count[winner] = loss_count.get(winner, 0)
+            loss_count[loser] = loss_count.get(loser, 0) + 1
         
-        for loss in loss_count:
-            if loss_count[loss] == 1:
-                second.append(loss)
+        no_loss, one_loss = [], []
+        for player, count in loss_count.items():
+            if count == 0:
+                no_loss.append(player)
+            if count == 1:
+                one_loss.append(player)
+                
+        return [sorted(no_loss), sorted(one_loss)]
         
-        return [ sorted(first), sorted(second) ]
