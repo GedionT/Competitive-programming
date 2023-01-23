90fc4d4f5eaa1f80052c3e1b02f899006ee1aec9
Gedion Teshome <Teshomegedion@gmail.com>
2023-01-23 13:03:00 +0300

    Time: 2725 ms (67.89%), Space: 70.6 MB (10.39%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,40 +1,23 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:     
-        """
-            1: 3                
-            2: 3
-            3: 6
-            5: 6, 7
-            4: 5, 8, 9
-            10: 4, 9
-
-            WINS(len(graph))   LOSS(count in values of all keys)
-            1:  1                0
-            2:  1                0
-            3:  1                2
-            4:  3                1
-            5:  2                1
-            10: 2                0
-            6:                   2
-            7:                   1
-            8:                   1
-            9:                   2
-
-            result = [[1, 2, 10], [4, 5, 7, 8]]
-        """ 
         
-        loss_count = {}
+        loss_count = [-1] * 100001
         
         for winner, loser in matches:
-            loss_count[winner] = loss_count.get(winner, 0)
-            loss_count[loser] = loss_count.get(loser, 0) + 1
-        
-        no_loss, one_loss = [], []
-        for player, count in loss_count.items():
-            if count == 0:
-                no_loss.append(player)
-            if count == 1:
-                one_loss.append(player)
+            if loss_count[winner] == -1:
+                loss_count[winner] = 0
+            
+            if loss_count[loser] == -1:
+                loss_count[loser] = 1
+            else:
+                loss_count[loser] += 1
                 
-        return [sorted(no_loss), sorted(one_loss)]
+        answer = [[], []]
+        for i in range(100001):
+            if loss_count[i] == 0:
+                answer[0].append(i)
+            elif loss_count[i] == 1:
+                answer[1].append(i)
+                
+        return answer
         
