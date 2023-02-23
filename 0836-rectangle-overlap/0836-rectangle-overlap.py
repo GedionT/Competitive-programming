0ff314c02b2656644e9a7d3e447cfdcfde5ee528
Gedion Teshome <Teshomegedion@gmail.com>
2023-02-23 22:14:24 +0300

    Time: 36 ms (37.21%), Space: 13.9 MB (54.37%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+
+        def intersect(p_left, p_right, q_left, q_right):
+            return min(p_right, q_right) > max(p_left, q_left)
+        
+        return (intersect(rec1[0], rec1[2], rec2[0], rec2[2]) and 
+                intersect(rec1[1], rec1[3], rec2[1], rec2[3]))    
