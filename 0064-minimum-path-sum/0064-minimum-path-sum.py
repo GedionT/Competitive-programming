67c99541a1c4c4843838537bd5de5a4f6e4d45d7
Gedion Teshome <Teshomegedion@gmail.com>
2023-03-27 06:40:14 +0300

    Time: 107 ms (37.87%), Space: 15.7 MB (80.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,27 +1,26 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
         ROW = len(grid)
         COL = len(grid[0])
         
-        directions = [(1,0), (0,1)]
-        isvalid = lambda x, y: 0 <= x < ROW and 0 <= y < COL 
-                
-        @cache
-        def dfs(x, y):
-            if x == ROW-1 and y == COL-1:
-                return grid[x][y]
-            
-            paths = []
-            for xi, yi in directions:
-                x2, y2 = x + xi, y + yi
-                
-                if isvalid(x2, y2):
-                    paths.append(dfs(x2, y2))
-                    
-            return grid[x][y] + min(paths)
-                    
-            
-        return dfs(0,0)
+        # Create a 2D table to store the minimum path sum for each cell
+        dp = [[0] * COL for _ in range(ROW)]
         
-         
+        # Initialize the bottom-right corner of the table
+        dp[ROW-1][COL-1] = grid[ROW-1][COL-1]
+        
+        # Initialize the bottom row of the table
+        for j in range(COL-2, -1, -1):
+            dp[ROW-1][j] = dp[ROW-1][j+1] + grid[ROW-1][j]
+        
+        # Initialize the rightmost column of the table
+        for i in range(ROW-2, -1, -1):
+            dp[i][COL-1] = dp[i+1][COL-1] + grid[i][COL-1]
+        
+        # Compute the minimum path sum for each cell starting from the second-to-last row and column
+        for i in range(ROW-2, -1, -1):
+            for j in range(COL-2, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
+        
+        # Return the minimum path sum for the top-left corner of the grid
+        return dp[0][0]
