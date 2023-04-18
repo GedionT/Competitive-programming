95dd4ee3fd2696d3b8759f280bdff69625a63550
Gedion Teshome <Teshomegedion@gmail.com>
2023-04-19 00:36:52 +0300

    Time: 40 ms (11.24%), Space: 13.8 MB (54.67%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,19 +4,15 @@ class Solution:
         p1 = p2 = 0
         builder = ""
         
-        while p1 < len(word1) and p2 < len(word2):
-            builder += word1[p1]
-            builder += word2[p2]
+        while p1 < len(word1) or p2 < len(word2):
             
-            p1 += 1
-            p2 += 1
-            
-        while p1 < len(word1):
-            builder += word1[p1]
-            p1 += 1
-            
-        while p2 < len(word2):
-            builder += word2[p2]
-            p2 += 1
+            if p1 < len(word1):
+                builder += word1[p1]
+                p1 += 1
+                
+            if p2 < len(word2):
+                builder += word2[p2]
+                p2 += 1
+  
             
         return builder
